2d0c1842832b8dc879ed6b0c4229dcec7afa5168
Carl Jackson <carl@avtok.com>
2014-05-16 21:49:04 -0700

    Simplify default targets
    We now only provide Stdout, and don't provide a convenience function for
    logging to an arbitrary os.File. In practice, I don't think this will be
    missed, since the logic for doing things like log rotation is a teensy
    bit tricky and didn't fit into the API I wanted to provide here.

--- targets.go
+++ targets.go
@@ -1,36 +1,36 @@
 package slog
 
 import (
+	"io"
 	"log"
 	"os"
 )
 
 const bufferSize = 100
 
+// Stdout is the default target of the root Logger. For performance, it is
+// fairly well buffered, and a goroutine is automatically spawned to read from
+// the channel and print to stdout.
 var Stdout chan<- string
-var Stderr chan<- string
 
 func init() {
-	Stdout = FileWriter(os.Stdout)
-	Stderr = FileWriter(os.Stderr)
-}
-
-func FileWriter(f *os.File) chan<- string {
 	ch := make(chan string, bufferSize)
-	go func() {
-		for line := range ch {
-			_, err := f.WriteString(line)
-			if err != nil {
-				// Using slog to log errors about slog seems...
-				// unwise.
-				log.Println("slog: while writing %q to %v: %v",
-					line, f, err)
-			}
-		}
-	}()
-	return ch
+	go stdoutWriter(ch)
+	Stdout = ch
 }
 
 func stdout(line map[string]interface{}) {
 	Stdout <- Format(line)
+}
+
+func stdoutWriter(ch <-chan string) {
+	for line := range ch {
+		_, err := os.Stdout.WriteString(line)
+		// Using slog to log errors about slog seems... nwise.
+		if err == io.ErrShortWrite {
+			log.Printf("slog: short write of %q", line)
+		} else if err != nil {
+			log.Printf("slog: error writing to stdout: %v", err)
+		}
+	}
 }
